19d118989acbbea1bc78cf22a15a24debc6a8a85
Nikhilkaleru <kalerutech@gmail.com>
2024-09-25 12:37:18 +0530

    Add files via upload

---
+++ src/main/java/zen/technopkg/App.java
@@ -0,0 +1,13 @@
+package zen.technopkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
